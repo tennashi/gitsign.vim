9c3945630d6e6f5a7b592d53cb88655ca26dbf33
tennashi <tennashio@gmail.com>
2023-02-18 21:28:15 +0900

    Upgrade deps.

--- denops/gitsign/deps.ts
+++ denops/gitsign/deps.ts
@@ -1,4 +1,4 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.11.3/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts#^";
 export { parse } from "https://deno.land/x/diff_parser@v0.2.0/mod.ts#^";
 export type { Diff } from "https://deno.land/x/diff_parser@v0.2.0/mod.ts#^";
-export { join } from "https://deno.land/std@0.107.0/path/mod.ts#^";
+export { join } from "https://deno.land/std@0.177.0/path/mod.ts#^";
